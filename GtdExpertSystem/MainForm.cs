beb2a85875ab9281210f08d655b4954899866821
Damian Ubowski <sverda300@gmail.com>
2019-01-21 21:33:33 +0100

    Setup user interface

---
+++ GtdExpertSystem/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace GtdExpertSystem
+{
+    public partial class MainForm : MaterialForm
+    {
+        private readonly MaterialSkinManager _skimManager;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _skimManager = MaterialSkinManager.Instance;
+            _skimManager.AddFormToManage(this);
+            _skimManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            _skimManager.ColorScheme = new ColorScheme(Primary.Green600, Primary.Green600, Primary.Green900, Accent.LightBlue200, TextShade.WHITE);
+        }
+    }
+}
